33364fcb8cf922d9fb3da9d3ac158ef0a6a93b2d
Cattail1 <premyerov96@gmail.com>
2020-09-23 01:51:40 +0300

    23.09.2020

---
+++ src/hm_1.java
@@ -0,0 +1,19 @@
+/*
+Написать программу, которая бы вывела в
+консоль звездочки следующим образом:
+*****
+*****
+*****
+*****
+ */
+public class hm_1 {
+    public static void main(String[] args){
+        for(int i=0; i<4; i++){
+            for(int j=0; j<5; j++){
+                System.out.print("*");
+            }
+            System.out.println("1");
+        }
+    }
+
+}
